7e224c2a39e7923f922b5106e97d0ba742ba21f1
seonno <lsno1106@gmail.com>
2021-05-14 00:56:35 +0900

    휴무, 병원명 포함

--- controllers/detail.js
+++ controllers/detail.js
@@ -8,49 +8,49 @@ exports.getDetail = async (req, res) => {
     let temp = "";
 
     temp =
-        data.dutyTime1s == "undefined"
+        data.dutyTime1s == undefined
             ? "휴무"
             : `${data.dutyTime1s}~${data.dutyTime1c}`;
     dutytime = dutytime.concat(`월(${temp}),`);
 
     temp =
-        data.dutyTime2s == "undefined"
+        data.dutyTime2s == undefined
             ? "휴무"
             : `${data.dutyTime2s}~${data.dutyTime2c}`;
     dutytime = dutytime.concat(`화(${temp}),`);
 
     temp =
-        data.dutyTime3s == "undefined"
+        data.dutyTime3s == undefined
             ? "휴무"
             : `${data.dutyTime3s}~${data.dutyTime3c}`;
     dutytime = dutytime.concat(`수(${temp}),`);
 
     temp =
-        data.dutyTime4s == "undefined"
+        data.dutyTime4s == undefined
             ? "휴무"
             : `${data.dutyTime4s}~${data.dutyTime4c}`;
     dutytime = dutytime.concat(`목(${temp}),`);
 
     temp =
-        data.dutyTime5s == "undefined"
+        data.dutyTime5s == undefined
             ? "휴무"
             : `${data.dutyTime5s}~${data.dutyTime5c}`;
     dutytime = dutytime.concat(`금(${temp}),`);
 
     temp =
-        data.dutyTime6s == "undefined"
+        data.dutyTime6s == undefined
             ? "휴무"
             : `${data.dutyTime6s}~${data.dutyTime6c}`;
     dutytime = dutytime.concat(`토(${temp}),`);
 
     temp =
-        data.dutyTime7s == "undefined"
+        data.dutyTime7s == undefined
             ? "휴무"
             : `${data.dutyTime7s}~${data.dutyTime7c}`;
     dutytime = dutytime.concat(`일(${temp}),`);
 
     temp =
-        data.dutyTime8s == "undefined"
+        data.dutyTime8s == undefined
             ? "휴무"
             : `${data.dutyTime8s}~${data.dutyTime8c}`;
     dutytime = dutytime.concat(`공휴일(${temp})`);
