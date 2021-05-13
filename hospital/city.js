74553f1ff86952b5d115f16159ccdaeb74328f99
changu100 <blue_rose17@naver.com>
2021-05-13 20:21:35 +0900

    시군구 작업 완료

--- hospital/city.js
+++ hospital/city.js
@@ -3,30 +3,31 @@ var log = require("../fs/logFileSave")
 var parser = require('xml2json');
 
 var hostpital = { };
-hostpital.getEmergencyMedicalCenter = function (Q0 = '서울특별시',Q1 = '강남구'){
+hostpital.getEmergencyMedicalCenter = function (Q0 = '서울특별시',Q1 = '강남구',pageNo = '',numOfRows = ''){
     var request_url = 'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire';
     var queryParams = '?' + encodeURIComponent('ServiceKey') + '=%2B0EDtslnfN6Uz8aAoFZq1qrwlfcXawVqrbJFFQEYvTYzvpdIm3ZpbtneOXOSBkcO4Y3%2BdoHkKs6u5VJuXFOxRg%3D%3D'; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('Q0') + '=' + encodeURIComponent(Q0); /* */
-    queryParams += '&' + encodeURIComponent('Q1') + '=' + encodeURIComponent(Q1); /* */
-    queryParams += '&' + encodeURIComponent('QT') + '=' + encodeURIComponent('1'); /* */
-    queryParams += '&' + encodeURIComponent('QZ') + '=' + encodeURIComponent('A'); /* */
-    queryParams += '&' + encodeURIComponent('QD') + '=' + encodeURIComponent('D000'); /* */
-    queryParams += '&' + encodeURIComponent('QN') + '=' + encodeURIComponent('(사)삼성생명공익재단 삼성서울병원'); /* */
-    queryParams += '&' + encodeURIComponent('ORD') + '=' + encodeURIComponent('NAME'); /* */
-    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
-    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+    if(Q0 != '')
+        queryParams += '&' + encodeURIComponent('Q0') + '=' + encodeURIComponent(Q0); /* */
+    if(Q0 != '')
+        queryParams += '&' + encodeURIComponent('Q1') + '=' + encodeURIComponent(Q1); /* */
+    if(pageNo != '')
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(pageNo); /* */
+    if(numOfRows != '')
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(numOfRows); /* */
   
     fetch(request_url + queryParams, {
         method: 'get'
     })
     .then(res =>{
-        console.log(res);
-        return res;
+        //console.log(res);
+        return res.text();
     })
     .then(data => {
-        
-        console.log(data)
-        return {data}
+        var json = parser.toJson(data);
+        console.log(data);
+        console.log("---------------------------");
+        console.log(json);
+        return json;
     });
     
 }
