0e235ec31275ccc19709a59153c186581fc6ddcd
changu100 <blue_rose17@naver.com>
2021-05-13 22:13:34 +0900

    axios data recieve

--- hospital/city.js
+++ hospital/city.js
@@ -1,36 +1,22 @@
-const fetch = require('node-fetch');
-var log = require("../fs/logFileSave")
-var parser = require('xml2json');
+const axios = require("axios");
 
-var hostpital = { };
-hostpital.getEmergencyMedicalCenter = function (Q0 = '서울특별시',Q1 = '강남구',pageNo = '',numOfRows = ''){
-    var request_url = 'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire';
-    var queryParams = '?' + encodeURIComponent('ServiceKey') + '=%2B0EDtslnfN6Uz8aAoFZq1qrwlfcXawVqrbJFFQEYvTYzvpdIm3ZpbtneOXOSBkcO4Y3%2BdoHkKs6u5VJuXFOxRg%3D%3D'; /* Service Key*/
-    if(Q0 != '')
-        queryParams += '&' + encodeURIComponent('Q0') + '=' + encodeURIComponent(Q0); /* */
-    if(Q0 != '')
-        queryParams += '&' + encodeURIComponent('Q1') + '=' + encodeURIComponent(Q1); /* */
-    if(pageNo != '')
-        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(pageNo); /* */
-    if(numOfRows != '')
-        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(numOfRows); /* */
-  
-    fetch(request_url + queryParams, {
-        method: 'get'
-    })
-    .then(res =>{
-        //console.log(res);
-        return res.text();
-    })
-    .then(data => {
-        var json = parser.toJson(data);
-        console.log(data);
-        console.log("---------------------------");
-        console.log(json);
-        return json;
-    });
-    
-}
-//hostpital.getEmergencyMedicalCenter();
+const centerInstance = axios.create({
+    baseURL: "http://apis.data.go.kr/B552657/ErmctInfoInqireService",
+});
 
-module.exports = hostpital;
+exports.getEmergencyMedicalCenter = async (Q0 = '',Q1='') => {
+    const serviceKey =
+        "%2B0EDtslnfN6Uz8aAoFZq1qrwlfcXawVqrbJFFQEYvTYzvpdIm3ZpbtneOXOSBkcO4Y3%2BdoHkKs6u5VJuXFOxRg%3D%3D";
+    //응급 의료기관 기본정보 조회
+    const uri = `/getEgytListInfoInqire?ServiceKey=${serviceKey}&Q0=${Q0}&Q1=${Q1}&numOfRows=100`;
+    const temp = await centerInstance.get(uri);
+    const info = temp.data.response.body;
+    console.log(info.items.item);
+
+    if (info.totalCount !== 1) {
+        //error
+    }
+    return info.items.item;
+};
+
+exports.getEmergencyMedicalCenter();
