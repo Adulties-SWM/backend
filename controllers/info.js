715dbd157f6ddfc58fb6b59b689b6df7059f4f88
seonno <lsno1106@gmail.com>
2021-05-13 20:28:22 +0900

    detail:오류수정

--- controllers/info.js
+++ controllers/info.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const convert = require('xml-js');
+const convert = require("xml-js");
 
 const emergencyInstance = axios.create({
     baseURL: "http://apis.data.go.kr/B552657/ErmctInfoInqireService",
@@ -7,13 +7,16 @@ const emergencyInstance = axios.create({
 
 exports.getHospitalInfo = async (hpid) => {
     const data = {
-        ServiceKey: "6QC9lid6DLI7BuAiFUYGyeKI0ApUM1jnvGawPV9K55PrmlEViLb4L%2BGc551Y6TpAURgFREbiTTJp2oTttTEsUw%3D%3D",
-        HPID = hpid,
-        pageNo = 1,
-        numberOfRows = 10,
+        ServiceKey:
+            "6QC9lid6DLI7BuAiFUYGyeKI0ApUM1jnvGawPV9K55PrmlEViLb4L%2BGc551Y6TpAURgFREbiTTJp2oTttTEsUw%3D%3D",
+        HPID: hpid,
+        pageNo: 1,
+        numberOfRows: 10,
     };
 
     //응급 의료기관 기본정보 조회
-    let data = await convert.toJson(emergencyInstance.get("/getEgytBassInfoInqire", data));
+    let data = await convert.toJson(
+        emergencyInstance.get("/getEgytBassInfoInqire", data)
+    );
     return data.body.items[0];
 };
