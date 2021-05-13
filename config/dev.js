1f570b53f377b64d424ff108359a68d5a0417561
jujube0 <60654009+jujube0@users.noreply.github.com>
2021-05-13 22:38:00 +0900

    Delete config directory

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  kakao_api_key : "9308cfd92a1e890f1569d69fecb4022d",
-  api_key: "t3pvHqBAiPPjhjP%2BQo35bNdJ8TEqcfab72vTB%2FNd71wBdJga6cVWvm1ND9Igizaua0VTmhl1JuaLLYtzZ5n2ag%3D%3D"
-}
